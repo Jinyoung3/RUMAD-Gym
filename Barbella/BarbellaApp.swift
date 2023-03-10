b8accb6c24bf20b62cc18be2c97229c9d3f051e2
Jinyoung3 <jinyoungoh9@gmail.com>
2023-03-09 19:25:03 -0500

    Initial Commit

---
+++ Barbella/BarbellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarbellaApp.swift
+//  Barbella
+//
+//  Created by Jinyoung Oh on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BarbellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barbella/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Barbella
+//
+//  Created by Jinyoung Oh on 3/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
